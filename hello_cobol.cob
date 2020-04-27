335761b7b4afbf594a400b0d3a690ad7cacb94c4
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2020-04-26 22:14:09 -0500

    Initial commit

---
+++ hello_cobol.cob
@@ -0,0 +1,14 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID. MYFirstCobolCode.
+AUTHOR. David G. Smith.
+*> I don't know what I want this to do yet
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+01  NameIn      PIC X(20).
+01  UserPrompt  PIC X(32) VALUE "Type your name then press RETURN".
+PROCEDURE DIVISION.
+   DISPLAY UserPrompt
+   ACCEPT NameIn
+   DISPLAY "Great job, ",NameIn
+   DISPLAY "You are a rock-star"
+   STOP RUN.
