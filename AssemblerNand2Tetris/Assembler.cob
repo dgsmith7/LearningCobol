276138f0f13d2361b307bf14214335ef14179950
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2020-05-10 13:09:19 -0500

    figured out how to lookup items from a record buffer

--- AssemblerNand2Tetris/Assembler.cob
+++ AssemblerNand2Tetris/Assembler.cob
@@ -37,12 +37,18 @@ FD PreDefTableFile.
 01 PreDefTable.
     02 PreDefInt PIC X(16).
     02 PreDefAsm PIC X(6).
-FD DataFile.  *>This is where the name of the file goes.  Will do user input name later.
-FD OutputFile. 
+*>FD DataFile.  *>This is where the name of the file goes.  Will do user input name later.
+*>FD OutputFile. 
 WORKING-STORAGE SECTION.
 01  LF PIC X.
 01  ROMAddress PIC 9(5) VALUE ZEROS.
 01  RAMAddress PIC 9(5) VALUE 16.
+01  DisplayLine.
+    02 AorC PIC XXX.
+    02 CompSect PIC X(7).
+    02 DestSect PIC XXX.
+    02 JumpSect PIC XXX.
+01  CompKey PIC X(7).
 *>01  UserDefTable.
 *>    02 UserDefSym OCCURS 500 TIMES X(80).
 *>    02 UserDefBin OCCURS 500 TIMES X(16).
@@ -76,6 +82,9 @@ Begin.
        READ CompTableFile
           AT END MOVE HIGH-VALUES TO CompTable
        END-READ
+     IF CompBin = "0001100" THEN 
+       DISPLAY "This one is the one"
+     END-IF
     END-PERFORM
     DISPLAY LF
     PERFORM UNTIL DestTable = HIGH-VALUES
@@ -100,6 +109,35 @@ Begin.
     END-PERFORM
     DISPLAY LF
     CLOSE CompTableFile
+    CLOSE DestTableFile
+    CLOSE JumpTableFile
+    CLOSE PreDefTableFile
+*> new stuff below
+*>   OPEN INPUT CompTableFile
+*>   READ CompTableFile
+*>     AT END MOVE HIGH-VALUES TO CompTable
+*>   END-READ
+*>   PERFORM UNTIL CompTable = HIGH-VALUES
+*>     IF CompAsm = 'D' THEN 
+*>       DISPLAY CompBin
+*>     END-IF
+*>     READ CompTableFile
+*>        AT END MOVE HIGH-VALUES TO CompTable
+*>     END-READ
+*>   END-PERFORM
+*>   CLOSE CompTableFile
+*>    MOVE '0001100' TO CompKey
+*>    READ CompTableFile CompBin IS CompKey
+*>        INVALID DISPLAY 'No such record'
+*>        NOT INVALID DISPLAY CompAsm
+*>    END-READ 
+
+*>    MOVE "1111" to AorC
+*>    MOVE CompBin (1) to CompSect
+*>    MOVE DestBin (1) to DestSect
+*>    MOVE JumpBin (1) to JumpSect 
+*>    DISPLAY DisplayLine
+
     STOP RUN.
 *>  Build compDestJumpPredef tables
 *>  first pass - if array has more lines advance
